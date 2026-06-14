4af16ee4fe477e62a3edd2002f6ff08ece3ff5a6
Utsav <kushwahautsav1511@gmail.com>
2026-06-14 16:17:58 +0530

    added cros

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ const submitRouter = require("./routes/submit");
 const cors = require("cors");
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL||"http://localhost:5173",
+  origin: process.env.FRONTEND_URL|| true,
   credentials:true
 }));
 
