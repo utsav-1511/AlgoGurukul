71a463380d996512a5309a76e95a8b0815fd9d16
Utsav <kushwahautsav1511@gmail.com>
2026-06-17 01:59:26 +0530

    hardcoded backend url

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: process.env.BACKEND_ADDRESS,
+  baseURL: "https://algogurukul.onrender.com",
   withCredentials:true,
   headers: {
     "Content-Type": "application/json"
