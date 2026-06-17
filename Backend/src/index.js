0a8595080c3636da7cde8fcac99be6ba33ad5052
Utsav <kushwahautsav1511@gmail.com>
2026-06-17 16:03:12 +0530

    fix URL

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,17 +10,20 @@ const submitRouter = require("./routes/submit");
 const cors = require("cors");
 const aiRouter = require("./routes/aiRouter");
 const  videoRouter =require("./routes/videoRouter");
-
+const axios = require("axios");
+axios.defaults.withCredentials = true;
 
 //new line
 app.set("trust proxy", 1);
 app.use(cors({
-  origin: ["https://algogurukul.netlify.app/","https://algo-gurukul.vercel.app/","http://localhost:5173/"],
+  origin: ["https://algogurukul.netlify.app","https://algo-gurukul.vercel.app","http://localhost:5173"],
   credentials: true
 }));
-
 app.use(express.json());
 app.use(cookieParser());
+app.get("/test", (req, res) => {
+  res.json({ success: true });
+});
 app.use("/user", authRouter);
 app.use("/problem", problemCreator);
 app.use("/submission",submitRouter);
